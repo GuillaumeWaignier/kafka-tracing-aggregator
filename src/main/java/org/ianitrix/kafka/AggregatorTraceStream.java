e148201cf86d6f4e9aa4b0ae49ebd3597697ff6a
Guillaume Waignier <guillaume.waignier@gmail.com>
2020-03-09 18:14:49 +0100

    feat(main): load configuration from file

--- src/main/java/org/ianitrix/kafka/AggregatorTraceStream.java
+++ src/main/java/org/ianitrix/kafka/AggregatorTraceStream.java
@@ -7,29 +7,48 @@ import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.errors.LogAndContinueExceptionHandler;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Properties;
 
+/**
+ * Main class
+ *
+ * @author Guillaume Waignier
+ *
+ */
 @Slf4j
 public class AggregatorTraceStream {
 
     public static void main(String[] args) {
-        log.info("Start Stream");
+        log.info("Starting ...");
+        final Instant startTime = Instant.now();
 
-        final Properties config = getStreamConfig();
+        checkArgument(args);
+
+        final Properties config = getDefaultStreamConfig();
+        config.putAll(loadConfigurationFile(args[0]));
+
         final Topology topology = new TraceTopologyStreamBuilder().buildStream();
         log.info(topology.describe().toString());
 
         final KafkaStreams streams = new KafkaStreams(topology, config);
-        streams.start();
         setupShutdownHook(streams);
+        streams.start();
+
+        final Instant endTime = Instant.now();
+        log.info("Started in {} ms", Duration.between(startTime, endTime).toMillis());
     }
 
-    public static Properties getStreamConfig() {
+    public static Properties getDefaultStreamConfig() {
         Properties settings = new Properties();
         settings.put(StreamsConfig.APPLICATION_ID_CONFIG, "_aggregator-trace-stream");
-        settings.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         settings.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.ByteArraySerde.class);
         settings.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.ByteArraySerde.class);
+        settings.put(StreamsConfig.TOPOLOGY_OPTIMIZATION, StreamsConfig.NO_OPTIMIZATION);
         settings.put(
                 StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG,
                 LogAndContinueExceptionHandler.class.getName());
@@ -41,5 +60,29 @@ public class AggregatorTraceStream {
             log.info("Stop stream");
             streams.close();
         }));
+    }
+
+    private static void checkArgument(final String[] args) {
+        if (args.length != 1) {
+            exitWithError("Missing argument for configuration file\nCommand line are : java - jar AggregatorTraceStream.jar config.properties", null);
+        }
+    }
+
+    public static Properties loadConfigurationFile(final String file) {
+        final File propertiesFile = new File(file);
+        final Properties properties = new Properties();
+
+        try (final FileInputStream inputStream = new FileInputStream(propertiesFile)) {
+            properties.load(inputStream);
+        } catch (final IOException e) {
+            exitWithError("Error when loading configuration file " + file, e);
+        }
+
+        return properties;
+    }
+
+    private static void exitWithError(final String errorMessage, final Exception exception) {
+        log.error(errorMessage, exception);
+        System.exit(1);
     }
 }
